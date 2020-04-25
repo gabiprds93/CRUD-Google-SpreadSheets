90cc8b8e2160714ff5d3d7aa26e1b4ac1fe4c527
Gabriela Paredes <gabiprds.93@gmail.com>
2020-04-25 17:49:22 -0500

    student data record

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $("#female").attr('checked', true);
+  $("#4to").attr('checked', true);
 });
 
 let script_url = "https://script.google.com/macros/s/AKfycbwb7sMr9uweR-7d2jxzCYvXMtEBYDuwkG7UQiRlExNeKEttgpJg/exec";
@@ -13,11 +14,20 @@ function insertValue(event) {
   data.nameTutor = $("#nameTutor").val();
   data.dniTutor =	$("#dniTutor").val();
   data.cellphoneTutor =	$("#cellphoneTutor").val();
-  data.gender = $('input:radio[name=gender]:checked').val();
+
+  data.lastNameStudent = $("#lastNameStudent").val();
+  data.mothersLastNameStudent = $("#mothersLastNameStudent").val();
+  data.nameStudent = $("#nameStudent").val();
+  data.dniStudent =	$("#dniStudent").val();
+  data.genderStudent = $('input:radio[name=genderStudent]:checked').val();
+  data.gradeStudent = $('input:radio[name=gradeStudent]:checked').val();
+  data.sectionStudent = $("#sectionStudent").val();
   data.selection=	$("select[name=options]").val();
 
   url = `${script_url}?lastNameTutor=${data.lastNameTutor}&nameTutor=${data.nameTutor}&dniTutor=${data.dniTutor}
-    &cellphoneTutor=${data.cellphoneTutor}&gender=${data.gender}&selection=${data.selection}&action=insert`;
+    &cellphoneTutor=${data.cellphoneTutor}&lastNameStudent=${data.lastNameStudent}&mothersLastNameStudent=${data.mothersLastNameStudent}
+    &nameStudent=${data.nameStudent}&dniStudent=${data.dniStudent}&genderStudent=${data.genderStudent}&gradeStudent=${data.gradeStudent}
+    &sectionStudent=${data.sectionStudent}&selection=${data.selection}&action=insert`;
   
   jQuery.ajax({
     crossDomain: true,
